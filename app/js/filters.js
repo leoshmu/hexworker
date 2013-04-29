b92fbe6a569e542365f16f51f9350bad4710843d
Leo Shmuylovich <leoshmu@gmail.com>
2013-04-29 17:19:03 -0400

    first commit

--- app/js/filters.js
+++ app/js/filters.js
@@ -2,9 +2,29 @@
 
 /* Filters */
 
-angular.module('myApp.filters', []).
+angular.module('hexWorker.filters', []).
   filter('interpolate', ['version', function(version) {
     return function(text) {
       return String(text).replace(/\%VERSION\%/mg, version);
     }
-  }]);
+  }]).
+  filter('hex2decimal', function(){
+    return function(string){
+      return parseInt(string, 16);
+    }
+  }).
+  filter('binary', function(){
+    return function(value){
+      return ((0xff+1)|value).toString(2).slice(1);
+    }
+  }).
+  filter('hexadecimal', function(){
+    return function(value){
+      return ((0xff+1)|value).toString(16).slice(1);
+    }
+  }).
+  filter('ascii', function(){
+    return function(value){
+      return String.fromCharCode(value);
+    }
+  });
